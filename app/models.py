f57fd035523e23da7c334824e7f9152c12a4b793
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-24 06:51:24 +0600

    Site is being ready

--- app/models.py
+++ app/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Sample(models.Model):
     image = models.ImageField(upload_to='images', blank=True)
 
+class Product(models.Model):
+    title = models.CharField(max_length=222)
+    desc = models.TextField()
+    product_image = models.ImageField(upload_to='images', blank=True)
