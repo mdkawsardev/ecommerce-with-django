035ef38877abce71eda7985c063e2abd992acb76
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-25 11:41:33 +0600

    Site is being ready

--- app/views.py
+++ app/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from .models import Product
+from math import ceil
 def index(request):
+    products = Product.objects.all()
+    n = len(products)
+    nSlide = n//4 + ceil((n/4) - (n//4))
     context = {
-        'products': Product.objects.all()
+        'products': products,
+        'no_of_slides': nSlide,
+        'range': range(1, nSlide)
     }
     return render(request, 'index.html', context)
+
+a = 12//4
+b = 12/4
+print(a)
+print(b)
+print(a - b)
+print(ceil(a - b))
+print()
+print(12//4 + ceil((12/4) - (12//4)))
